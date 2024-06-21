303f6b18a077b5bd6a83f68afa1bedf52f6dccfb
Bhushan <bhushanmayekar5@gmail.com>
2024-06-21 15:10:28 +0530

    ip change

--- src/util.js
+++ src/util.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const GetStates = (setState) => {
-    axios.get("http://localhost:9000/states").then((res)=>{
+    axios.get("http://3.110.159.106:9000/states").then((res)=>{
         // console.log(res);
         setState(res.data.data) 
         localStorage.setItem("states",JSON.stringify(res.data.data)) 
@@ -10,7 +10,7 @@ export const GetStates = (setState) => {
 }
 
 export const GetDistrict = (scd,setState) => {
-    axios.get(`http://localhost:9000/district/${scd}`).then((res)=>{
+    axios.get(`http://3.110.159.106:9000/district/${scd}`).then((res)=>{
         console.log(res.data.data);
         setState(res.data.data)  
         localStorage.setItem("district",JSON.stringify(res.data.data))
@@ -19,7 +19,7 @@ export const GetDistrict = (scd,setState) => {
 }
 
 export const  GetAc = (scd,dcd,setState) => {
-    axios.get(`http://localhost:9000/constituencies/${scd}/${dcd}`).then((res)=>{
+    axios.get(`http://3.110.159.106:9000/constituencies/${scd}/${dcd}`).then((res)=>{
         console.log(res.data);
         setState(res.data.data)  
         localStorage.setItem("constituencies",JSON.stringify(res.data.data))
@@ -29,7 +29,7 @@ export const  GetAc = (scd,dcd,setState) => {
 
 export const  GetPart = (scd,dcd,acn,setState,setCurPart) => {
     let curIndex = localStorage.getItem("index");
-    axios.get(`http://localhost:9000/partlist/${scd}/${dcd}/${acn}`).then((res)=>{
+    axios.get(`http://3.110.159.106:9000/partlist/${scd}/${dcd}/${acn}`).then((res)=>{
         setState(res.data.data.payload);
         let partListJ = res.data.data.payload
         let part = partListJ && Object.entries(partListJ)[parseInt(curIndex)][1]
@@ -39,25 +39,25 @@ export const  GetPart = (scd,dcd,acn,setState,setCurPart) => {
 }
 
 export const PdfDownload = async (stateName,districtName,acName,partName,body) => {
-    let response = await axios.post(`http://localhost:9000/downloadPdf/${stateName}/${districtName}/${acName}/${partName}`, body).then((res)=>{
+    let response = await axios.post(`http://3.110.159.106:9000/downloadPdf/${stateName}/${districtName}/${acName}/${partName}`, body).then((res)=>{
         return res.data
     })
     return response
 }
 
 export const GetCaptcha = (setCaptcha) => {
-    axios.get("http://localhost:9000/getcaptcha").then((res)=>{
+    axios.get("http://3.110.159.106:9000/getcaptcha").then((res)=>{
         console.log(res.data);
         setCaptcha(res.data.data)
     })
 }
 
 export const logError = (parameters) => {
-    axios.post('http://localhost:9000/logerror',parameters);
+    axios.post('http://3.110.159.106:9000/logerror',parameters);
 }
 
 export const GetErr = (setState) => {
-    axios.get('http://localhost:9000/allerror').then((res)=>{
+    axios.get('http://3.110.159.106:9000/allerror').then((res)=>{
         let data = res.data.data
         let master = []
         data.forEach(ele=>{
@@ -71,7 +71,7 @@ export const GetErr = (setState) => {
 
 export const GetErrorData = (err,setError,setCur) =>{
     let curIndex = localStorage.getItem("index");
-    axios.post('http://localhost:9000/errordata',{file: err}).then((res)=>{
+    axios.post('http://3.110.159.106:9000/errordata',{file: err}).then((res)=>{
         let data = res.data.data;
         setError(data)
         let part = data && Object.entries(data)[parseInt(curIndex)][1]
